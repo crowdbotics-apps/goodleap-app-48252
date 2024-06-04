60d8b9f09ba2e834174662c8adcb259771817cc7
Crowdbotics <team@crowdbotics.com>
2024-06-04 15:20:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "goodleap-app-48252",
+  url: "https://goodleap-app-48252.botics.co",
+  api: "https://goodleap-app-48252.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
